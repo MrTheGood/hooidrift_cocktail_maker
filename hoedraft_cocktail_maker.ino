d28a2293fcdbcb0038952285bb68ffc5011e0c8e
MrTheGood <maarten@degoedeweb.nl>
2021-12-19 18:53:10 +0100

    Setup code for cocktail making

--- hoedraft_cocktail_maker.ino
+++ hoedraft_cocktail_maker.ino
@@ -18,62 +18,82 @@ void setup() {
 }
 
 
-int index = 0;
-bool isButtonUpPressed = false;
-bool isButtonDownPressed = false;
-bool isButtonSelectPressed = false;
-
+signed int index = 0;
+unsigned long startTime = 0;
 bool isMakingCocktail = false;
-int tmp = 0;
 
 
 void loop() {
-
-  if (isMakingCocktail) {
-    if (tmp % 2 == 0) digitalWrite(index + 5, HIGH);
-    else digitalWrite(index + 5, LOW);
-    tmp++;
-
-    if (tmp >= 500) {
-      tmp = 0;
-      isMakingCocktail = false;
-    }
-  } else {
-    digitalWrite(index + 5, LOW);
-
   
-    int buttonUpValue = digitalRead(BUTTON_UP);
-    if (buttonUpValue == HIGH) {
-      if (!isButtonUpPressed) {
-        isButtonUpPressed = true;
-        index = (index + 1) % 5;
-      }
-    } else isButtonUpPressed = false;
-  
-    
-    int buttonDownValue = digitalRead(BUTTON_DOWN);
-    if (buttonDownValue == HIGH) {
-      if (!isButtonDownPressed) {
-        isButtonDownPressed = true;
-        index = (index - 1) % 5;
-        if (index == -1) index = 4;
-      }
-    } else isButtonDownPressed = false;
-  
-    
-    int buttonSelectValue = digitalRead(BUTTON_SELECT);
-    if (buttonSelectValue == HIGH) {
-      if (!isButtonSelectPressed) {
-        isButtonSelectPressed = true;
-        isMakingCocktail = true;
-      }
-    } else isButtonSelectPressed = false;
-
-    
-    digitalWrite(index + 5, HIGH);
-  }
-
-
+  if (isMakingCocktail) makeCocktail();
+  else selection();
 
   delay(20);
+}
+
+
+void selection() {
+  static bool isButtonUpPressed = false;
+  static bool isButtonDownPressed = false;
+  static bool isButtonSelectPressed = false;
+
+  
+  digitalWrite(index + 5, LOW);
+  
+  
+  int buttonUpValue = digitalRead(BUTTON_UP);
+  if (buttonUpValue == HIGH) {
+    if (!isButtonUpPressed) {
+      isButtonUpPressed = true;
+      index = (index + 1) % 5;
+    }
+  } else isButtonUpPressed = false;
+  
+  
+  int buttonDownValue = digitalRead(BUTTON_DOWN);
+  if (buttonDownValue == HIGH) {
+    if (!isButtonDownPressed) {
+      isButtonDownPressed = true;
+      index--;
+      if (index < 0) index = 4;
+    }
+  } else isButtonDownPressed = false;
+  
+  
+  int buttonSelectValue = digitalRead(BUTTON_SELECT);
+  if (buttonSelectValue == HIGH) {
+    if (!isButtonSelectPressed) {
+      isButtonSelectPressed = true;
+      isMakingCocktail = true;
+      startTime = millis();
+    }
+  } else isButtonSelectPressed = false;
+  
+  
+  digitalWrite(index + 5, HIGH);
+}
+
+
+void makeCocktail() {
+  const long flashInterval = 250;
+  const long cocktailDuration = 2500;
+
+  
+  static unsigned long previousTime = 0;
+  static bool isSelectedLampOn = false;
+
+  
+  unsigned long now = millis();
+  
+  if (isSelectedLampOn) digitalWrite(index + 5, HIGH);
+  else digitalWrite(index + 5, LOW);
+
+  if (now - previousTime > flashInterval) {
+    isSelectedLampOn = !isSelectedLampOn;
+  }
+  
+  if (now - startTime > cocktailDuration) {
+    isMakingCocktail = false;
+    startTime = 0;
+  }
 }
