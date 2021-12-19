34c4ae17b6dbb725e7ee263c508fae59565c239b
MrTheGood <maarten@degoedeweb.nl>
2021-12-19 15:35:11 +0100

    Add buttonUp reverse-selection

--- hoedraft_cocktail_maker.ino
+++ hoedraft_cocktail_maker.ino
@@ -1,9 +1,13 @@
 
-
-
+const int BUTTON_UP = 2;
+const int BUTTON_DOWN = 3;
+const int BUTTON_SELECT = 4;
 
 
 void setup() {
+  pinMode(BUTTON_UP, INPUT);
+  pinMode(BUTTON_DOWN, INPUT);
+  pinMode(BUTTON_SELECT, INPUT);
 
   // Selection LEDs
   pinMode(5, OUTPUT);
@@ -13,13 +17,26 @@ void setup() {
   pinMode(9, OUTPUT);
 }
 
-int index = 0;
-void loop() {
-  digitalWrite(index, LOW);
-  
-  index = (index + 1) % 5 + 5;
 
-  digitalWrite(index, HIGH);
+int index = 0;
+bool isButtonUpPressed = false;
+
+
+void loop() {
+  digitalWrite(index + 5, LOW);
+
   
-  delay(100);
+  int buttonUpValue = digitalRead(BUTTON_UP);
+
+  if (buttonUpValue == HIGH) {
+    if (!isButtonUpPressed) {
+      isButtonUpPressed = true;
+      index = (index + 1) % 5;
+    }
+  } else isButtonUpPressed = false;
+  
+
+
+  digitalWrite(index + 5, HIGH);
+  delay(20);
 }
